a6f6c71371f37d5cdbf7deefcafdd09e60957b11
fernando-nunes21 <nmgamesforfun@gmail.com>
2021-06-19 04:29:53 -0300

    add Class Start

---
+++ class/User.php
@@ -0,0 +1,13 @@
+<?php
+
+    class User{
+        private $idUser;
+        private $username;
+        
+        function a(){
+            return $this->idUser;
+        }
+    }
+
+    //Continue creating User Class and others
+?>
